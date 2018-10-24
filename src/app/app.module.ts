c448c4a0700454e31794234c06911ede39dd410d
Jad Chahine <jad.chahine4@gmail.com>
2018-10-24 22:28:08 +0300

    Add filter component, list component and edit component, show them in the same page using flexLayout, add routing in order to let /cameras redirect to the base html page, install flexLayout inside the project alongwith angular cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
